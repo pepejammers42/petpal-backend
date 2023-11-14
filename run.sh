3a8e5f0454d00b86b1b8165dca00bc199d7f6730
eric <sun_eric@outlook.com>
2023-11-14 00:41:45 -0500

    initial project setup

---
+++ run.sh
@@ -0,0 +1,7 @@
+#
+#/bin/bash
+
+source ./venv/bin/activate
+python3 ./manage.py makemigrations
+python3 ./manage.py migrate
+# python3 ./manage.py runserver
---
+++ startup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# UPDATE APT
+sudo apt update
+
+# PYTHON
+if ! command -v python3 &> /dev/null/; then
+  echo "Installing python..."
+  sudo apt install python3.10
+  sudo apt install python3-venv
+fi
+
+# PIP
+if ! command -v pip3 &> /dev/null; then
+    echo "Installing pip..."
+    sudo apt install python3-pip
+fi
+
+# VENV
+echo "Creating virtual environment..."
+python3 -m venv venv
+source ./venv/bin/activate
+
+# INSTALL REQ PACKAGES
+echo "Installing required packages..."
+pip install django djangorestframework Pillow djangorestframework-simplejwt
+
+# MIGRATE
+echo "Running Django management commands..."
+python3 ./manage.py makemigrations
+python3 ./manage.py migrate
+
+echo "Completed."
