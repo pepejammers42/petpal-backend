701acc41aa241a11f2ef19c64f6c4ebb570f7c67
eric <sun_eric@outlook.com>
2023-11-16 23:10:22 -0500

    sad typo fix

--- petpal/petpal/settings.py
+++ petpal/petpal/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_AUTHENTIFICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
