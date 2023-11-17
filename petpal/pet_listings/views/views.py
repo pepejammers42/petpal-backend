845b8d5b7e45cafad6fac9e9cb4fab6927ff6557
LennyGradient <jeremyli.xie@mail.utoronto.ca>
2023-11-16 20:59:02 -0500

    Update Create and Listing Logic

--- petpal/pet_listings/views/views.py
+++ petpal/pet_listings/views/views.py
@@ -20,10 +20,51 @@ class PetListingCreateAPIView(CreateAPIView):
     serializer_class = PetListingSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def perform_create(self, serializer):
+        # self is the shelter
+        # status should be available at the time of creation
+        serializer.save(shelter=self, status="available")
+
 class PetListingListAPIView(ListAPIView):
     queryset = PetListing.objects.all()
     serializer_class = PetListingSerializer
     permission_classes = [permissions.AllowAny] # All pet listings can be viewed, regardless of status, unless they are deleted.
+
+    def get_queryset(self):
+        queryset = PetListing.objects.all()
+
+        # Retrieve filter parameters
+        shelter = self.request.query_params.get('shelter', None)
+        status = self.request.query_params.get('status', None)
+        breed = self.request.query_params.get('breed', None)
+        age = self.request.query_params.get('age', None)
+        size = self.request.query_params.get('size', None)
+        color = self.request.query_params.get('color', None)
+        gender = self.request.query_params.get('gender', None)
+
+        # Apply sorting based on parameters
+        if shelter:
+            queryset = queryset.filter(shelter__name__icontains=shelter)
+
+        if status:
+            queryset = queryset.filter(status=status)
+
+        if breed:
+            queryset = queryset.filter(breed__icontains=breed)
+
+        if age:
+            queryset = queryset.filter(age=age)
+
+        if size:
+            queryset = queryset.filter(size=size)
+
+        if color:
+            queryset = queryset.filter(color__icontains=color)
+
+        if gender:
+            queryset = queryset.filter(gender=gender)
+
+        return queryset
 
 class PetListingUpdateAPIView(UpdateAPIView):
     queryset = PetListing.objects.all()
