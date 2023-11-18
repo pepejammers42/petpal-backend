77774c7e946b8f54add9df0cfa9d94cfeb3b5daa
jiho <jiho@J_laptop.localdomain>
2023-11-18 16:43:38 -0500

    applications first commit

---
+++ petpal/applications/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from ..petlistings.models import PetListing
+from ..accounts.models import Seeker
+
+class Application(models.Model):
+    STATUS_CHOICES = [
+        'pending',
+        'accepted',
+        'denied',
+        'withdrawn'
+    ]
+
+    pet_listing = models.ForeignKey(PetListing, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(Seeker)
+    status = models.CharField(choices=STATUS_CHOICES, default='pending', max_length=20)
+    creation_time = models.DateTimeField(default=timezone.now)
+    last_update_time = models.DateTimeField(default=timezone.now)
+    personal_statement = models.TextField()
+
