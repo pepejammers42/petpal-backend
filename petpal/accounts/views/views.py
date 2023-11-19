e6cbca455afe9b3995401fdf2ee69150255c8c86
eric <sun_eric@outlook.com>
2023-11-18 19:08:33 -0500

    check if seeker has app with shelter implented, needs testing

--- petpal/accounts/views/views.py
+++ petpal/accounts/views/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.core.exceptions import PermissionDenied
 
 from ..models import Shelter, Seeker
 from ..serializers import ShelterSerializer, SeekerSerializer
@@ -24,9 +25,21 @@ class SeekerRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Seeker.objects.all()
     serializer_class = SeekerSerializer
     permission_classes = [IsAuthenticated]
-
+    
     def get_object(self):
-        return get_object_or_404(Seeker, pk=self.kwargs['pk'])
+        
+        seeker = get_object_or_404(Seeker, pk=self.kwargs['pk'])
+        
+        if isinstance(self.request.user, Shelter):
+            # TODO: Check if this works!
+            check_app = Applications.objects.filter(applicant=seeker, pet_listing__shelter=self.request.user).exists()
+
+            if check_app:
+                return seeker
+            else:
+                raise PermissionDenied("You do not have permission to view this user's profile.")
+        return seeker
+        
 
 class ShelterRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Shelter.objects.all()
--- petpal/api/urls.py
+++ petpal/api/urls.py
@@ -6,7 +6,6 @@ from rest_framework_simplejwt.views import (
 from django.urls import path
 
 urlpatterns = [
-    # path('token/', LoginView.as_view(), name='token_obtain_pair'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
