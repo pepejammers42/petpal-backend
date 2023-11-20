70a70532fef755e4e529bf70d29da170c2eeb603
hknguyen20 <20nguyen.hk@vinuni.edu.vn>
2023-11-19 22:37:37 -0500

    tmp

--- petpal/comments/urls.py
+++ petpal/comments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ShelterCommentCreateView,  ShelterCommentListView
 
 urlpatterns = [
-    # path('<int:application_id>/comments/', ),
-    # path('<int:application_id>/comment/<int:comment_id>/', ),
-    path('shelters/<int:shelter_id>/comments/list/', ShelterCommentListView.as_view()),
+    path('applications/<int:application_id>/comments/all/', ),
+    path('applications/int:application_id>/comment/', ),
+    path('shelters/<int:shelter_id>/comments/all/', ShelterCommentListView.as_view()),
     path('shelters/<int:shelter_id>/comments/', ShelterCommentCreateView.as_view()),
 ]
--- petpal/comments/views.py
+++ petpal/comments/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from rest_framework import generics, permissions
 from .models import Comment
 from accounts.models import Shelter, AuthUser
-# from ..applications.models import Application
+from applications.models import Application
 from django.contrib.contenttypes.models import ContentType
 from .serializers import CommentSerializer
 
@@ -25,4 +25,24 @@ class ShelterCommentListView(generics.ListAPIView):
     def get_queryset(self):
         shelter_id = self.kwargs['shelter_id']
         content_type = ContentType.objects.get_for_model(Shelter)
-        return Comment.objects.filter(content_type=content_type, object_id=shelter_id).order_by('-created_at')
+        return Comment.objects.filter(content_type=content_type, object_id=shelter_id).order_by('-created_at') #descending order
+
+class ApplicationCommentCreateView(generics.CreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        shelter_id = self.kwargs['shelter_id']
+        shelter = get_object_or_404(Shelter, pk=shelter_id)
+        content_type = ContentType.objects.get_for_model(Shelter)
+        serializer.save(user=self.request.user, object_id=shelter_id, content_type=content_type)
+                                            
+
+class ShelterCommentListView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        shelter_id = self.kwargs['shelter_id']
+        content_type = ContentType.objects.get_for_model(Shelter)
+        return Comment.objects.filter(content_type=content_type, object_id=shelter_id).order_by('-created_at') #descending order
