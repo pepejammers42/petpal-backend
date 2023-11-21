f0d7630fcb4ee9093727233a609d399dd5601f71
eric <sun_eric@outlook.com>
2023-11-21 17:38:25 -0500

    some more changes

--- petpal/comments/permissions.py
+++ petpal/comments/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 
 class IsApplicationRelated(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated()
+        return request.user.is_authenticated
     def has_object_permission(self, request, view, obj):
         return obj.shelter == request.user or obj.seeker == request.user
--- petpal/comments/urls.py
+++ petpal/comments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ShelterCommentCreateView,  ShelterCommentListView, ApplicationCommentCreateView, ApplicationCommentListView
 
 urlpatterns = [
-    path('applications/<int:application_id>/comments/all/', ApplicationCommentListView.as_view()),
+    path('applications/<int:application_id>/', ApplicationCommentListView.as_view()),
     path('applications/<int:application_id>/comment/', ApplicationCommentCreateView.as_view()),
-    path('shelters/<int:shelter_id>/comments/all/', ShelterCommentListView.as_view()),
+    path('shelters/<int:shelter_id>/', ShelterCommentListView.as_view()),
     path('shelters/<int:shelter_id>/comments/', ShelterCommentCreateView.as_view()),
 ]
