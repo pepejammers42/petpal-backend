4866b3568d08b2da9b54d435990ce907c54c7391
LennyGradient <jeremyli.xie@mail.utoronto.ca>
2023-11-21 16:51:51 -0500

    Update Notifications Model, Setup URLs and Add Structure For View Code

--- petpal/notifications/urls.py
+++ petpal/notifications/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import NotificationCreate, NotificationRetrieveUpdateDestroy
 
 urlpatterns = [
-    
+    path('', NotificationCreate.as_view(), name='pet-listing-list-create'),
+    path('<int:pk>/', NotificationRetrieveUpdateDestroy.as_view(), name='pet-listing-retrieve-update-destroy'),
 ]
--- petpal/notifications/views.py
+++ petpal/notifications/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
+class NotificationCreate(ListCreateAPIView):
+    def get_queryset(self):
+        pass
+    
+    def perform_create(self, serializer):
+        pass
+
+class NotificationRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    def get_object(self):
+        pass
